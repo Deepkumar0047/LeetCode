a6d52871331b5bdbdfc0f3721996ac58b219bedb
Deepkumar0047 <deepk636328@gmail.com>
2026-07-23 21:35:49 +0530

    Time: 3 ms (89.94%) | Memory: 11.8 MB (95.64%) - LeetSync

---
+++ 227-basic-calculator-ii/basic-calculator-ii.cpp
@@ -0,0 +1,38 @@
+class Solution {
+    public:
+        int calculate(string s) {
+            long long num = 0;
+            long long last = 0;
+            long long result = 0;
+            char op = '+';
+
+            for (int i = 0; i < s.size(); i++) {
+                if (isdigit(s[i])) {
+                    num = num * 10 + (s[i] - '0');
+                }
+
+                if ((!isdigit(s[i]) && s[i] != ' ') || i == s.size() - 1) {
+
+                    if (op == '+') {
+                        result += last;
+                        last = num;
+                    }
+                    else if (op == '-') {
+                        result += last;
+                        last = -num;
+                    }
+                    else if (op == '*') {
+                        last = last * num;
+                    }
+                    else if (op == '/') {
+                        last = last / num;
+                    }
+
+                    op = s[i];
+                    num = 0;
+                }
+            }
+
+            return result + last;
+        }
+};
