76d38d71c39a92bf2d98f4d416ea90cf89db97c2
Deepkumar0047 <deepk636328@gmail.com>
2026-09-14 23:54:19 +0530

    Time: 14 ms (11.13%) | Memory: 20.3 MB (17.24%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -1,37 +1,48 @@
 class Solution {
-    public:
-        bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+public:
+    bool dfs(int node, vector<vector<int>>& adj, 
+             vector<int>& visited, vector<int>& path) {
+        
+        visited[node] = 1;
+        path[node] = 1;
 
-            vector<vector<int>> adj(numCourses);
-            vector<int> indegree(numCourses, 0);
+        for (int next : adj[node]) {
+            
+            if (path[next] == 1)
+                return false;
 
-            for (auto &p : prerequisites) {
-                adj[p[1]].push_back(p[0]);
-                indegree[p[0]]++;
+            if (visited[next] == 0) {
+                if (!dfs(next, adj, visited, path))
+                    return false;
             }
-
-            queue<int> q;
-
-            for (int i = 0; i < numCourses; i++) {
-                if (indegree[i] == 0)
-                q.push(i);
-            }
-
-            int count = 0;
-
-            while (!q.empty()) {
-                int node = q.front();
-                q.pop();
-                count++;
-
-                for (int neighbour : adj[node]) {
-                    indegree[neighbour]--;
-
-                    if (indegree[neighbour] == 0)
-                    q.push(neighbour);
-                }
-            }
-
-            return count == numCourses;
         }
+
+        path[node] = 0;
+
+        return true;
+    }
+
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        
+        vector<vector<int>> adj(numCourses);
+
+        for (auto p : prerequisites) {
+            int course = p[0];
+            int prerequisite = p[1];
+
+            adj[prerequisite].push_back(course);
+        }
+
+        vector<int> visited(numCourses, 0);
+        vector<int> path(numCourses, 0);
+
+        for (int i = 0; i < numCourses; i++) {
+            if (visited[i] == 0) {
+                if (!dfs(i, adj, visited, path))
+                    return false;
+            }
+        }
+
+        return true;
+    }
 };
